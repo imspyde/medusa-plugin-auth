43572298c6644cec72ca6bd4085a492bba85ef26
Maxime Poitevineau-Millin <maxime@p1001.fr>
2022-12-14 15:56:09 +0100

    fix

--- loaders/index.js
+++ loaders/index.js
@@ -6,11 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const google_1 = __importDefault(require("../auth-strategies/google"));
 const facebook_1 = __importDefault(require("../auth-strategies/facebook"));
 const linkedin_1 = __importDefault(require("../auth-strategies/linkedin"));
+const auth0_1 = __importDefault(require("../auth-strategies/auth0"));
 async function authStrategiesLoader(container, authOptions) {
     const configModule = container.resolve('configModule');
     google_1.default.load(container, configModule, authOptions);
     facebook_1.default.load(container, configModule, authOptions);
     linkedin_1.default.load(container, configModule, authOptions);
+    auth0_1.default.load(container, configModule, authOptions);
 }
 exports.default = authStrategiesLoader;
 //# sourceMappingURL=index.js.map
